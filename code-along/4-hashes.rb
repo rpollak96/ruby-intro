9e79e08694b80e3bc2f7f8c8c4052843c8653649
rpollak96 <rachael.pollak@kellogg.northwestern.edu>
2026-01-29 02:31:19 +0000

    hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,27 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {"name" => "Rachael", "location" => "New York", "status" => "learning"}
+# puts profile
 
 # Accessing data from the hash
+name = profile["name"]
+# puts "Hello, #{name}"
+
+profile["age"] = 29
+# puts profile
+
+profile["name"] = "Ray"
+# puts profile
 
 # More Complex Hashes
+
+profile["location"] = {"city" => "New York", "state" => "NY"}
+# puts profile["location"]["state"]
+
+timeline = [{"status" => "learning", "occured_at" => "9:30pm"}, {"status" => "sleeping", "occured_at" => "10:30pm"}]
+
+profile["timeline"] = timeline
+puts profile
+puts "right now"
+puts profile["timeline"][0]["status"]
